94f46a763bb8591c76e60911c4806631fe64692d
Mateq438 <mateq97438@gmail.com>
2026-05-20 08:03:56 +0200

    premier commit

---
+++ lib/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA9Tc1QnzPDKXTgKbwkhUiJ8sgIIjDvNS8",
+  authDomain: "pea-tracker-87d61.firebaseapp.com",
+  projectId: "pea-tracker-87d61",
+  storageBucket: "pea-tracker-87d61.firebasestorage.app",
+  messagingSenderId: "440523082502",
+  appId: "1:440523082502:web:cbb9ebc1971c231439ac87",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
